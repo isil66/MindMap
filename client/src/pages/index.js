284d3fd28a5fb208af631b0ea89aa61be9b6b0d8
Işıl <isilozgu2016@gmail.com>
2024-07-10 23:18:58 +0300

    color grading

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -4,39 +4,42 @@ import styles from "@/styles/Home.module.css"; // If you have any additional sty
 
 export default function Home() {
     return (
-        <div className={styles.container}>
-            <Head>
-                <link rel="preconnect" href="https://fonts.googleapis.com"/>
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap"/>
-            </Head>
-            <div className={styles.typeAnimationContainer}>
-                <TypeAnimation
-                    preRenderFirstString={true}
-                    sequence={[
-                        500,
-                        'Your Second Brain \n for editing',
-                        1000,
-                        'Your Second Brain \n for journaling',
-                        1000,
-                        'Your Second Brain \n for remembering',
-                        1000,
-                        'Be Unconventional',
-                        1000,
-                        'Be You',
-                        1000,
-                    ]}
-                    speed={50}
-                    style={{
-                        fontSize: '5em',
-                        fontFamily: 'Playfair Display, serif',
-                        whiteSpace: 'pre-line',
-                        textAlign: 'center',
-                    }}
-                    repeat={Infinity}
-                />
+        <div className={styles.pageContainer}>
+            <div className={styles.container}>
+                <Head>
+                    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap"/>
+                </Head>
+                <div className={styles.typeAnimationContainer}>
+                    <TypeAnimation
+                        preRenderFirstString={true}
+                        sequence={[
+                            500,
+                            'Your Second Brain \n for editing',
+                            1000,
+                            'Your Second Brain \n for journaling',
+                            1000,
+                            'Your Second Brain \n for remembering',
+                            1000,
+                            'Be Unconventional',
+                            1000,
+                            'Be You',
+                            1000,
+                        ]}
+                        speed={50}
+                        style={{
+                            fontSize: '5em',
+                            fontFamily: 'Playfair Display, serif',
+                            whiteSpace: 'pre-line',
+                            textAlign: 'center',
+                        }}
+                        repeat={Infinity}
+                    />
+                </div>
             </div>
         </div>
-    );
+    )
+        ;
 }
