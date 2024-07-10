db89c12a3778f6364458371b90b3dc12b15114bd
Işıl <isilozgu2016@gmail.com>
2024-07-10 22:16:03 +0300

    styling animation

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,36 +1,41 @@
 import Head from "next/head";
-import Image from "next/image";
-import {Inter} from "next/font/google";
-import styles from "@/styles/Home.module.css";
 import {TypeAnimation} from "react-type-animation";
-import {useState} from "react";
-
-const inter = Inter({subsets: ["latin"]});
+import styles from "@/styles/Home.module.css"; // If you have any additional styles
 
 export default function Home() {
-
     return (
         <div>
+            <Head>
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap"/>
+            </Head>
             <TypeAnimation
                 preRenderFirstString={true}
                 sequence={[
                     500,
-                    'Your Second Brain for editing',
+                    'Your Second Brain \n for editing',
                     1000,
-                    'Your Second Brain for journaling',
+                    'Your Second Brain \n for journaling',
                     1000,
-                    'Your Second Brain for remembering',
+                    'Your Second Brain \n for remembering',
                     1000,
-                    'Be unconventional',
+                    'Be Unconventional',
                     1000,
-                    'Be you',
-                    500,
+                    'Be You',
+                    1000,
                 ]}
                 speed={50}
-                style={{fontSize: '2em'}}
+                style={{
+                    fontSize: '4em',
+                    fontFamily: 'Playfair Display, serif',
+                    whiteSpace: 'pre-line',
+                    height: '195px',
+                    display: 'block'
+                }}
                 repeat={Infinity}
             />
-
         </div>
     );
 }
