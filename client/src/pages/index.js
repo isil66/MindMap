97020a87f9db06f73b3f282330ff936e8d17c2d1
Işıl <isilozgu2016@gmail.com>
2024-07-10 22:29:57 +0300

    styling animation center top

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -4,38 +4,39 @@ import styles from "@/styles/Home.module.css"; // If you have any additional sty
 
 export default function Home() {
     return (
-        <div>
+        <div className={styles.container}>
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com"/>
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
                 <link
                     href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap"/>
             </Head>
-            <TypeAnimation
-                preRenderFirstString={true}
-                sequence={[
-                    500,
-                    'Your Second Brain \n for editing',
-                    1000,
-                    'Your Second Brain \n for journaling',
-                    1000,
-                    'Your Second Brain \n for remembering',
-                    1000,
-                    'Be Unconventional',
-                    1000,
-                    'Be You',
-                    1000,
-                ]}
-                speed={50}
-                style={{
-                    fontSize: '4em',
-                    fontFamily: 'Playfair Display, serif',
-                    whiteSpace: 'pre-line',
-                    height: '195px',
-                    display: 'block'
-                }}
-                repeat={Infinity}
-            />
+            <div className={styles.typeAnimationContainer}>
+                <TypeAnimation
+                    preRenderFirstString={true}
+                    sequence={[
+                        500,
+                        'Your Second Brain \n for editing',
+                        1000,
+                        'Your Second Brain \n for journaling',
+                        1000,
+                        'Your Second Brain \n for remembering',
+                        1000,
+                        'Be Unconventional',
+                        1000,
+                        'Be You',
+                        1000,
+                    ]}
+                    speed={50}
+                    style={{
+                        fontSize: '5em',
+                        fontFamily: 'Playfair Display, serif',
+                        whiteSpace: 'pre-line',
+                        textAlign: 'center',
+                    }}
+                    repeat={Infinity}
+                />
+            </div>
         </div>
     );
 }
