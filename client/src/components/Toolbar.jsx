a2ff8a296cc2be0b8da7f4ae70b7924f015316c2
Işıl <isilozgu2016@gmail.com>
2024-07-17 13:54:28 +0300

    initial note field

--- client/src/components/Toolbar.jsx
+++ client/src/components/Toolbar.jsx
@@ -1,7 +1,11 @@
 "use client";
 
-import React, {useEffect, useRef} from "react";
+import React, {useState, useEffect, useRef} from "react";
 import {Editor} from "@tiptap/react";
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
 import styles from "../styles/Tiptap.module.css";
 import {
   Bold,
@@ -19,34 +23,28 @@ import {
   Redo,
   Code
 } from "lucide-react";
-import IconButton from '@mui/material/IconButton';
-import Tooltip from '@mui/material/Tooltip';
 
 const BASE_URL = process.env.NEXT_PUBLIC_DJANGO_API_BASE_URL;
 
 const Toolbar = ({editor, content}) => {
   const noteIdRef = useRef(0);// todo DB'den çek
   const takeNoteRef = useRef(true);
-
+  const [showTextField, setShowTextField] = useState(false);
+  const [noteContent, setNoteContent] = useState('');
+  const [textFieldPosition, setTextFieldPosition] = useState({top: 0, left: 0});
 
   const getNoteIdStartingPoint = async () => {
 	try {
 	  const storedToken = localStorage.getItem('authToken');
-
 	  const response = await fetch(`${BASE_URL}/dashboard/page/notes/`, {
 		method: 'GET',
 		headers: {
 		  Authorization: `Token ${storedToken}`,
 		},
 	  });
-
-
-	  if (response.ok)  {
+	  if (response.ok) {
 		const responseJson = await response.json();
-		console.log(responseJson);
-		console.log(responseJson.largest_current_note_id);
 		noteIdRef.current = responseJson.largest_current_note_id;
-
 	  }
 	} catch (error) {
 	  console.error('Error fetching dashboard data:', error.message);
@@ -54,7 +52,7 @@ const Toolbar = ({editor, content}) => {
   };
 
   useEffect(() => {
-	getNoteIdStartingPoint()
+	getNoteIdStartingPoint();
   }, []);
 
   if (!editor) {
@@ -64,29 +62,24 @@ const Toolbar = ({editor, content}) => {
   const handleClickTakeNoteStart = (e) => {
 	e.preventDefault();
 	takeNoteRef.current = false;
-
 	const newNoteId = noteIdRef.current + 1;
 	noteIdRef.current = newNoteId;
-
-
 	editor.chain().focus().toggleHighlight({color: "#500bb6", note_id: newNoteId}).run(); // Add color options
   };
 
   const handleClickTakeNoteEnd = (e) => {
 	e.preventDefault();
 	takeNoteRef.current = true;
-
-
-	editor.chain().focus().toggleHighlight({color: "#500bb6", note_id: noteIdRef.current}).run(); // Add color options
+	editor.chain().focus().toggleHighlight({color: "#500bb6", note_id: noteIdRef.current}).run();
+	showTextFieldAtCursor();
   };
 
   const selectToTakeNote = (e) => {
 	const newNoteId = noteIdRef.current + 1;
 	noteIdRef.current = newNoteId;
 	editor.chain().focus().toggleHighlight({color: "#500bb6", note_id: newNoteId}).run();
+	showTextFieldAtCursor();
   };
-
-
 
   const handleNoteToggles = (e) => {
 	if (editor.state.selection['ranges'][0]['$to']['pos']
@@ -99,150 +92,200 @@ const Toolbar = ({editor, content}) => {
 	}
   };
 
+  const showTextFieldAtCursor = () => {
+	const {from, to} = editor.state.selection;//bu da prosemirrorden
+	const start = editor.view.coordsAtPos(from);//prosemirror view api'ından bunlar
+	const end = editor.view.coordsAtPos(to);
+
+	const top = (start.top + end.top) / 2 - 30;
+	const left = (start.left + end.left) / 2;
+	setTextFieldPosition({top, left});
+	setShowTextField(true);
+  };
+
+  const handleCancel = () => {
+	setShowTextField(false);
+	setNoteContent('');
+  };
+
+  const handleSave = () => {
+	// todo: post requesti at
+	console.log('Saved note content:', noteContent);
+	setShowTextField(false);
+	setNoteContent('');
+  };
+
   const buttonStyle = {
 	margin: "5px",
 	color: "#621d9a",
   };
 
   return (
-	<div style={{display: "flex", justifyContent: "center", margin: "10px 0"}} className={styles.toolbar}>
-	  <Tooltip title="Bold" placement="top">
-		<IconButton
-		  style={buttonStyle}
-		  onClick={(e) => {
-			e.preventDefault();
-			editor.chain().focus().toggleBold().run();
-		  }}>
-		  <Bold/>
-		</IconButton>
-	  </Tooltip>
-	  <Tooltip title="Italic" placement="top">
-		<IconButton
-		  style={buttonStyle}
-		  onClick={(e) => {
-			e.preventDefault();
-			editor.chain().focus().toggleItalic().run();
-		  }}>
-		  <Italic/>
-		</IconButton>
-	  </Tooltip>
-	  <Tooltip title="Underline" placement="top">
-		<IconButton
-		  style={buttonStyle}
-		  onClick={(e) => {
-			e.preventDefault();
-			editor.chain().focus().toggleUnderline().run();
-		  }}>
-		  <Underline/>
-		</IconButton>
-	  </Tooltip>
-	  <Tooltip title="Strikethrough" placement="top">
-		<IconButton
-		  style={buttonStyle}
-		  onClick={(e) => {
-			e.preventDefault();
-			editor.chain().focus().toggleStrike().run();
-		  }}>
-		  <Strikethrough/>
-		</IconButton>
-	  </Tooltip>
-	  <Tooltip title="Highlight" placement="top">
-		<IconButton
-		  style={buttonStyle}
-		  onClick={(e) => {
-			e.preventDefault();
-			editor.chain().focus().toggleHighlight().run();
-		  }}>
-		  <Highlighter/>
-		</IconButton>
-	  </Tooltip>
-	  <Tooltip title="Take Note" placement="top">
-		<IconButton
-		  style={buttonStyle}
-		  onClick={handleNoteToggles}>
-		  <StickyNote/>
-		</IconButton>
-	  </Tooltip>
-	  <Tooltip title="Heading 1" placement="top">
-		<IconButton
-		  style={buttonStyle}
-		  onClick={(e) => {
-			e.preventDefault();
-			editor.chain().focus().toggleHeading({level: 1}).run();
-		  }}>
-		  <Heading1/>
-		</IconButton>
-	  </Tooltip>
-	  <Tooltip title="Heading 2" placement="top">
-		<IconButton
-		  style={buttonStyle}
-		  onClick={(e) => {
-			e.preventDefault();
-			editor.chain().focus().toggleHeading({level: 2}).run();
-		  }}>
-		  <Heading2/>
-		</IconButton>
-	  </Tooltip>
-	  <Tooltip title="Heading 3" placement="top">
-		<IconButton
-		  style={buttonStyle}
-		  onClick={(e) => {
-			e.preventDefault();
-			editor.chain().focus().toggleHeading({level: 3}).run();
-		  }}>
-		  <Heading3/>
-		</IconButton>
-	  </Tooltip>
-	  <Tooltip title="Bullet List" placement="top">
-		<IconButton
-		  style={buttonStyle}
-		  onClick={(e) => {
-			e.preventDefault();
-			editor.chain().focus().toggleBulletList().run();
-		  }}>
-		  <List/>
-		</IconButton>
-	  </Tooltip>
-	  <Tooltip title="Ordered List" placement="top">
-		<IconButton
-		  style={buttonStyle}
-		  onClick={(e) => {
-			e.preventDefault();
-			editor.chain().focus().toggleOrderedList().run();
-		  }}>
-		  <ListOrdered/>
-		</IconButton>
-	  </Tooltip>
-	  <Tooltip title="Code" placement="top">
-		<IconButton
-		  style={buttonStyle}
-		  onClick={(e) => {
-			e.preventDefault();
-			editor.chain().focus().setCode().run();
-		  }}>
-		  <Code/>
-		</IconButton>
-	  </Tooltip>
-	  <Tooltip title="Undo" placement="top">
-		<IconButton
-		  style={buttonStyle}
-		  onClick={(e) => {
-			e.preventDefault();
-			editor.chain().focus().undo().run();
-		  }}>
-		  <Undo/>
-		</IconButton>
-	  </Tooltip>
-	  <Tooltip title="Redo" placement="top">
-		<IconButton
-		  style={buttonStyle}
-		  onClick={(e) => {
-			e.preventDefault();
-			editor.chain().focus().redo().run();
-		  }}>
-		  <Redo/>
-		</IconButton>
-	  </Tooltip>
+	<div>
+	  <div style={{display: "flex", justifyContent: "center", margin: "10px 0"}} className={styles.toolbar}>
+		<Tooltip title="Bold" placement="top">
+		  <IconButton
+			style={buttonStyle}
+			onClick={(e) => {
+			  e.preventDefault();
+			  editor.chain().focus().toggleBold().run();
+			}}>
+			<Bold/>
+		  </IconButton>
+		</Tooltip>
+		<Tooltip title="Italic" placement="top">
+		  <IconButton
+			style={buttonStyle}
+			onClick={(e) => {
+			  e.preventDefault();
+			  editor.chain().focus().toggleItalic().run();
+			}}>
+			<Italic/>
+		  </IconButton>
+		</Tooltip>
+		<Tooltip title="Underline" placement="top">
+		  <IconButton
+			style={buttonStyle}
+			onClick={(e) => {
+			  e.preventDefault();
+			  editor.chain().focus().toggleUnderline().run();
+			}}>
+			<Underline/>
+		  </IconButton>
+		</Tooltip>
+		<Tooltip title="Strikethrough" placement="top">
+		  <IconButton
+			style={buttonStyle}
+			onClick={(e) => {
+			  e.preventDefault();
+			  editor.chain().focus().toggleStrike().run();
+			}}>
+			<Strikethrough/>
+		  </IconButton>
+		</Tooltip>
+		<Tooltip title="Highlight" placement="top">
+		  <IconButton
+			style={buttonStyle}
+			onClick={(e) => {
+			  e.preventDefault();
+			  editor.chain().focus().toggleHighlight().run();
+			}}>
+			<Highlighter/>
+		  </IconButton>
+		</Tooltip>
+		<Tooltip title="Take Note" placement="top">
+		  <IconButton
+			style={buttonStyle}
+			onClick={handleNoteToggles}>
+			<StickyNote/>
+		  </IconButton>
+		</Tooltip>
+		<Tooltip title="Heading 1" placement="top">
+		  <IconButton
+			style={buttonStyle}
+			onClick={(e) => {
+			  e.preventDefault();
+			  editor.chain().focus().toggleHeading({level: 1}).run();
+			}}>
+			<Heading1/>
+		  </IconButton>
+		</Tooltip>
+		<Tooltip title="Heading 2" placement="top">
+		  <IconButton
+			style={buttonStyle}
+			onClick={(e) => {
+			  e.preventDefault();
+			  editor.chain().focus().toggleHeading({level: 2}).run();
+			}}>
+			<Heading2/>
+		  </IconButton>
+		</Tooltip>
+		<Tooltip title="Heading 3" placement="top">
+		  <IconButton
+			style={buttonStyle}
+			onClick={(e) => {
+			  e.preventDefault();
+			  editor.chain().focus().toggleHeading({level: 3}).run();
+			}}>
+			<Heading3/>
+		  </IconButton>
+		</Tooltip>
+		<Tooltip title="Bullet List" placement="top">
+		  <IconButton
+			style={buttonStyle}
+			onClick={(e) => {
+			  e.preventDefault();
+			  editor.chain().focus().toggleBulletList().run();
+			}}>
+			<List/>
+		  </IconButton>
+		</Tooltip>
+		<Tooltip title="Ordered List" placement="top">
+		  <IconButton
+			style={buttonStyle}
+			onClick={(e) => {
+			  e.preventDefault();
+			  editor.chain().focus().toggleOrderedList().run();
+			}}>
+			<ListOrdered/>
+		  </IconButton>
+		</Tooltip>
+		<Tooltip title="Code" placement="top">
+		  <IconButton
+			style={buttonStyle}
+			onClick={(e) => {
+			  e.preventDefault();
+			  editor.chain().focus().setCode().run();
+			}}>
+			<Code/>
+		  </IconButton>
+		</Tooltip>
+		<Tooltip title="Undo" placement="top">
+		  <IconButton
+			style={buttonStyle}
+			onClick={(e) => {
+			  e.preventDefault();
+			  editor.chain().focus().undo().run();
+			}}>
+			<Undo/>
+		  </IconButton>
+		</Tooltip>
+		<Tooltip title="Redo" placement="top">
+		  <IconButton
+			style={buttonStyle}
+			onClick={(e) => {
+			  e.preventDefault();
+			  editor.chain().focus().redo().run();
+			}}>
+			<Redo/>
+		  </IconButton>
+		</Tooltip>
+	  </div>
+	  {showTextField && (
+		<div style={{
+		  position: 'absolute',
+		  top: textFieldPosition.top,
+		  left: textFieldPosition.left,
+		  zIndex: 1000,
+		  background: '#fff',
+		  padding: '10px',
+		  borderRadius: '5px',
+		  boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)'
+		}}>
+		  <TextField
+			multiline
+			rows={4}
+			variant="outlined"
+			value={noteContent}
+			onChange={(e) => setNoteContent(e.target.value)}
+			style={{width: '300px'}}
+		  />
+		  <div style={{marginTop: '10px', textAlign: 'right'}}>
+			<Button onClick={handleCancel} style={{marginRight: '10px'}}>Cancel</Button>
+			<Button onClick={handleSave} variant="contained" color="primary">Save</Button>
+		  </div>
+		</div>
+	  )}
 	</div>
   );
 };
